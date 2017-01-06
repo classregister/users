4c2d653245eb5c1c2a9d74dbff0b588eb5651177
Krzysztof Janiec <krzysztof.janiec@poczta.pl>
2017-01-06 18:10:39 +0100

    ClassRegister Users - exception handler

---
+++ src/test/java/ovh/classregister/users/exception/ExceptionHandlerTest.java
@@ -0,0 +1,46 @@
+package ovh.classregister.users.exception;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import ovh.classregister.users.web.UserController;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ExceptionHandlerTest {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private UserController userController;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(userController)
+                                      .setControllerAdvice(new ExceptionHander())
+                                      .build();
+    }
+
+    @Test
+    public void shouldReturnProblemJsonWhenResourceNotFoundExceptionIsThrown() throws Exception {
+        long userId = 1;
+        when(userController.getUser(userId)).thenThrow(new ResourceNotFoundException(userId));
+
+        mockMvc.perform(get("/users/{id}", userId))
+               .andExpect(status().isNotFound())
+               .andExpect(jsonPath("$.type").value("/users/" + userId))
+               .andExpect(jsonPath("$.title").value("User does not exist"))
+               .andExpect(jsonPath("$.detail").value(String.format("User with id: %d not found", userId)))
+               .andExpect(jsonPath("$.status").value("NOT_FOUND"));
+    }
+
+
+}
